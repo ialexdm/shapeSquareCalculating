81545655fcb6a335abe44f6037110942c2feca13
Aleksandr <ivashinin.alexander@ya.ru>
2022-09-29 00:27:53 +0300

    Add ShapeFactory, ShapeService and ShapeSquareCalculator

---
+++ ShapeSquareCalculating/ShapeSquareCalculator/ShapeSquareCalculator.cs
@@ -0,0 +1,16 @@
+﻿using ShapeSquareCalculating.figures;
+
+namespace ShapeSquareCalculating.Application
+{
+    public class ShapeSquareCalculator
+    {
+        public double CalculateSquare(double radius)
+        {
+            return ShapeFactory.GetShape(radius).Square;
+        }
+        public double CalculateSquare(double aSide, double bSide, double cSide)
+        {
+            return ShapeFactory.GetShape(aSide,bSide,cSide).Square;
+        }
+    }
+}
